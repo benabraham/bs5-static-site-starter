b727803aee8ab25cf9db26749e2ace439b8edc1d
DanielSrb <srb.dan@gmail.com>
2021-05-24 14:34:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# A basic template to develop a website based on Bootstrap 5
+
+Includes:
+
+-   **Bootstrap 5**: build responsive, mobile-first projects on the web with the world's most popular front-end component library
+-   **Gulp 4**: task runner for running all of the following
+-   **Sass compilation**: leverage the power of the most popular CSS extension language
+-   **Sourcemaps** generation for easier Sass debugging
+-   **Browsersync**: automatically reloads (or injects in case of CSS), browsers' when you change files
+-   **Autoprefixer**: parses CSS and adds vendor prefixes according to [caniuse.com]()
+-   **UnCSS**: removes unused styles from CSS
+-   **CSSO**: CSS minifier with structural optimizations
+-   **Nunjucks**: templating language by Mozilla
+-   **Surge.sh**: deploy static websites easily and for free
+
+## First time installation
+
+### Install latest [node.js](https://nodejs.org/)
+
+### Install all packages from `package.json` locally
+
+```shell
+npm install
+```
+
+_If you’re having errors with `node-gyp`, try [installing it globally](https://github.com/nodejs/node-gyp#installation)._
+
+## Development
+
+To develop with automatic compilation and browser refreshing run
+
+```shell
+npm start
+```
+
+And see the result on `http://localhost:3000/`
+
+## Build
+
+To build everything once for production deploy (in `/dist/` folder)
+
+This build uses all generated HTML files for _UnCSS_. If it removes something you need to keep, add and array to `ignore` option in `gulpfile.esm.js`.
+
+```shell
+npm run build
+```
+
+## CSS (Sass preprocessor)
+
+`index.css` is compiled from `src/scss/index.scss` by [Sass](http://sass-lang.com/).
+
+You don't know _Scss_ syntax or don't want to use it? Just use plain CSS in `src/scss/_custom-css.scss`.
+
+## HTML (Nunjucks templates)
+
+HTML is generated from [Nunjucks](https://mozilla.github.io/nunjucks/) templates in `src/templates`.
+
+You don't need to leverage the power of templates. You can just create plain HTML files with `*.njk` extension.
+
+If you don't want a template to be turned into HTML file start it with `_`. Typically these are templates used for _include_ or _extend_.
+
+[Documentation for Nunjucks](https://mozilla.github.io/nunjucks/templating.html).
+
+If you need some data to be available in all templates, use `templates/data.json` for that.
+
+## Static files (JavaScript, images, …)
+
+Folders and files from `/src/static/` are simply copied directly to `/dist/` folder.
+
+### Bootstrap
+
+You can comment out Bootstrap components you don't need in `/src/index.scss`.
+
+`/src/_custom-bootstrap-variables.scss` contains only customized Bootstrap variables.
+
+See `browserslist` in `package.json` for supported browsers.
+
+### Deployment
+
+Upload everything in `/dist/` folder to the server.
+
+#### Surge.sh
+
+You can use [surge.sh](https://surge.sh) free service for that.
+
+1. If you don’t have a surge account: run surge client with `npx surge` in `/dist` folder to create it.
+1. Set your own domain in `package.json` (just replace `https://my-first-bootstrap-5-website.surge.sh` with yours).
+1. From now on run `npm run deploy` whenever you want to publish a new version.
+
+If you want multiple people to be able to deploy to the same domain, run `surge --add mail.your.collaborator.used.to.register.with.surge@example.com` for each.
---
+++ gulpfile.esm.js
@@ -0,0 +1,139 @@
+// FIRST LOAD EVERYTHING NEEDED…
+const { series, parallel, src, dest, watch } = require('gulp'),
+    { readFileSync } = require('fs'),
+    del = require('del'),
+    sass = require('gulp-dart-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    uncss = require('postcss-uncss'),
+    csso = require('gulp-csso'),
+    nunjucks = require('gulp-nunjucks'),
+    browserSync = require('browser-sync').create();
+
+// DEFINE FUNCTIONS
+
+// 1) functions to delete parts of generated files in dist folder
+
+// delete all files
+const allCleanup = () => del('dist/**/*');
+
+// delete all HTML files
+const htmlCleanup = () => del('dist/**/*.html');
+
+// delete all CSS files and their sourcemaps
+const cssCleanup = () => del('dist/*.{css,css.map}');
+
+// delete static files
+const staticCleanup = () => del(
+    [
+        'dist/**/*', // delete all files from /dist/
+        '!dist/**/*.{html,css,css.map}', // except HTML, CSS and CSS map files
+    ],
+    { onlyFiles: true } // do not delete folders (would delete all folders otherwise)
+);
+
+// 2) functions that generate files
+
+// compile Nunjucks templates to html files
+const htmlCompile = () => src('src/templates/**/[^_]*.njk')
+    // import data from data.json
+    .pipe(nunjucks.compile(JSON.parse(String(readFileSync('src/data.json')))))
+    .pipe(dest('./dist/')); // put compiled html into dist folder
+
+// create and process CSS
+const sassCompile = () => src('src/scss/index.scss') // this is the source of for compilation
+    .pipe(sourcemaps.init()) // initalizes a sourcemap
+    .pipe(sass.sync().on('error', sass.logError)) // compile SCSS to CSS and also tell us about a problem if happens
+    .pipe(
+        postcss([
+            autoprefixer, // automatically adds vendor prefixes if needed
+            // see browserslist in package.json for included browsers
+            // Official Bootstrap browser support policy:
+            // https://getbootstrap.com/docs/5.0/getting-started/browsers-devices/#supported-browsers
+        ])
+    )
+    .pipe(csso()) // compresses CSS
+    .pipe(sourcemaps.write('./')) // writes the sourcemap
+    .pipe(dest('./dist')) // destination of the resulting CSS
+    .pipe(browserSync.stream()); // tell browsersync to inject compiled CSS
+
+// remove unused CSS (classes not used in generated HTML)
+const removeUnusedCss = () => src('dist/index.css')
+    .pipe(
+        postcss([
+            uncss({
+                html: ['dist/**/*.html'],
+                media: ['print'], // process additional media queries
+                ignore: [], // provide a list of selectors that should not be removed by UnCSS
+            }),
+        ])
+    )
+    .pipe(dest('dist'));
+
+// copy all files from /src/static/ to /dist/static/
+const copyStatic = () => src('src/static/**/*').pipe(dest('dist'))
+
+// 3) functions to watch and serve
+// development with automatic refreshing after changes to CSS, templates or static files
+const startBrowsersync = () => browserSync.init({ // initalize Browsersync
+    // port: 8080, // set different port
+    // open: false, // don’t open browser
+    // ghostMode: false, // CLICKS, scrolls & form inputs on any device will not be mirrored to all others
+    // reloadOnRestart: true, // reload each browser when Browsersync is restarted
+    server: {
+        baseDir: 'dist', // serve from this folder
+        serveStaticOptions: {
+            // trying an extension when one isn't specified:
+            // effectively means that http://localhost:3000/another-page
+            // shows file named another-page.html
+            extensions: ['html'],
+        },
+    },
+});
+
+// a function to reload Browsersync
+const reloadBrowserSync = (cb) => {
+    browserSync.reload();
+    cb();
+};
+
+// a function to watch for changes
+const watchFiles = () => {
+    // SCSS changed: run task to compile it again
+    watch('src/scss/**/*', processCss);
+    // templates or data file changed: run task to generate HTML again and reload
+    watch(['src/templates/**/*', 'src/data.json'], series(processHtml, reloadBrowserSync));
+    // static files changed: run task to copy them again and reload
+    watch('src/static/**/*', series(processStatic, reloadBrowserSync));
+}
+
+// COMPOSE TASKS
+
+const processHtml = series(htmlCleanup, htmlCompile);
+
+const processCss = series(cssCleanup, sassCompile);
+
+const processStatic = series(staticCleanup, copyStatic);
+
+// EXPORT PUBLICLY AVAILABLE TASKS
+// These tasks can be run with `npx gulp TASKNAME` on command line for example `npx gulp develop`.
+// We use them in npm scripts with `gulp TASKNAME` (see package.json).
+
+// development with automatic refreshing
+exports.develop = series(
+    allCleanup,
+    parallel(htmlCompile, sassCompile, copyStatic),
+    parallel(startBrowsersync, watchFiles)
+);
+
+// build everything for production
+exports.build = series(
+    allCleanup,
+    htmlCompile,
+    parallel(sassCompile, copyStatic),
+    removeUnusedCss
+);
+
+// the default task runs when you run just `gulp`
+exports.default = exports.develop;
